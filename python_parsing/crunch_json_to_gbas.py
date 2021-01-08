c32b7223e388f4ac331e60310ebaa029091a748a
jurkstas <jurkstas1@yahoo.com>
2021-01-08 13:12:53 +0100

    Simplify the crunch and add .gasm and .gt1 for reference

--- python_parsing/crunch_json_to_gbas.py
+++ python_parsing/crunch_json_to_gbas.py
@@ -3,31 +3,8 @@
 # an Arduino program
 
 import json
-import struct
-import binascii
 
-hexToBinaryDict = { \
-"0": "0000", \
-"1": "0001", \
-"2": "0010", \
-"3": "0011", \
-"4": "0100", \
-"5": "0101", \
-"6": "0110", \
-"7": "0111", \
-"8": "1000", \
-"9": "1001", \
-"a": "1010", \
-"b": "1011", \
-"c": "1100", \
-"d": "1101", \
-"e": "1110", \
-"f": "1111" \
-}
 
-binaryToHexDict = {}
-for k, v in hexToBinaryDict.items():
-  binaryToHexDict[v] = k
 
 f = open('connectome.json', 'r')
 
@@ -62,9 +39,11 @@ rawWeightList = []
 rawNameList = []
 rawUberNameList = []
 
-i = 0
+
 for neuron in linkedNeurons:
   conn = connectomeDict[neuron]
+  print('dim ', end="", flush=True)
+  print(neuron + '(' + str(len(conn)-1) + ') =', end="", flush=True)
   #romIndexList.append(binascii.hexlify(struct.pack('>H', i)))
   rawRomIndexList.append(i)
 
@@ -75,69 +54,16 @@ for neuron in linkedNeurons:
     rawNameList.append(connNeuron)
     rawWeightList.append(weight)
     rawUberNameList.append(neuron)
+    
+    indexWeight = weight * 512 + connIndex
+  #  print(weight)
+  #  print(connIndex)
+    print(hex(abs(indexWeight))+ ',', end="", flush=True) #this is WRONG because sign is necessary
 
-    lowConnIndex = connIndex & 0x00FF
-    hiConnIndex = connIndex >> 8 # This is 1 or 0, depending on whether it's > 255
-    #if connIndex > 255:
-      #hiConnIndex = 1
-    #else:
-      #hiConnIndex = 0
+  print()
 
-    firstByte = binascii.hexlify(struct.pack('B', lowConnIndex))
+print('dim neurons(' + str(len(linkedNeurons)-1) + ') = ', end="", flush=True)
+for neuron in linkedNeurons:
+  print('@' + neuron + ',', end="", flush=True)
 
-    weightByte = binascii.hexlify(struct.pack('b', weight))
-
-    secondByte = list(weightByte)
-    print(weight)
-    print(weightByte)
-    print(secondByte)
-    secondByte = [hexToBinaryDict[x] for x in secondByte]
-    secondByte = ''.join(secondByte)
-    secondByte = list(secondByte)
-
-    secondByte[0] = str(hiConnIndex)
-
-    secondByte = ''.join(secondByte)
-
-    secondByte = binascii.hexlify(struct.pack('B', int(secondByte, 2)))
-
-    romConnList.append(firstByte+secondByte)
-
-    if neuron == "AVL":
-      print(connIndex, connNeuron, weight)
-
-    i += 1
-
-rawRomIndexList.append(i)
-romIndexListLen = len(rawRomIndexList)
-
-linkedNeuronListLength = binascii.hexlify(struct.pack('>H', len(linkedNeurons)))
-
-romIndexList = [binascii.hexlify(struct.pack('>H', x+romIndexListLen+1)) for x in rawRomIndexList]
-
-totalRomWords = [linkedNeuronListLength] + romIndexList + romConnList
-
-print("Connectome assembled from " + str(len(totalRomWords)*2) + " bytes")
-
-with open('c_array.out', 'w') as output:
-  output.write('const uint16_t PROGMEM NeuralROM[] = {\n')
-  i = 0
-  for j in range(len(totalRomWords)):
-    if j == (len(totalRomWords)-1):
-      output.write('0x'+totalRomWords[j]+'\n};')
-    else:
-      output.write('0x'+totalRomWords[j]+', ')
-    i += 1
-
-    if i == 15:
-      output.write('\n')
-      i = 0
-
-print("WORD OF INTEREST:")
-print(totalRomWords[1530])
-
-with open('c_defines.out', 'w') as output:
-  i = 0
-  for neuron in connectomeList:
-    output.write('#define N_' + neuron + ' ' + str(i) + '\n')
-    i += 1
+print(unlinkedNeurons)
